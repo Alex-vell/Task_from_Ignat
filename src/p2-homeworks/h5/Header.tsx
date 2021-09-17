0099f33d59835a8d061f6e7fca3bd3770843f61a
Alex-vell <https://github.com/Alex-vell/Git-My-first-project.git>
2021-09-18 02:08:16 +0300

    homeworks 5

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {PATH} from "./Routes";
+import s from './Header.module.css'
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
+        <div className={s.nav}>
+
+            <span className={s.item}>
+                <NavLink to={PATH.PRE_JUNIOR} activeClassName={s.active}>Pre-junior </NavLink>
+            </span>
+            <span className={s.item}>
+                <NavLink to={PATH.JUNIOR} activeClassName={s.active}>Junior </NavLink>
+            </span>
+            <span className={s.item}>
+                <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.active}>Junior+ </NavLink>
+            </span>
+            <div className={s.level}>{'Level >'}</div>
 
         </div>
     )
---
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Error404 from "./Error404";
+
+export function Junior() {
+    return (
+        <div>
+            <Error404/>
+        </div>
+    )
+}
+
+
+// сделайте по аналогии пустые страницы джун и джун+
+// туда будут добавляться следующие дз
